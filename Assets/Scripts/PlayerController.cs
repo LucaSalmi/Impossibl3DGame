70cf6471ca6694464e794c3d3174999992f6b082
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-10-04 15:57:55 +0200

    created jump logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,9 @@ public class PlayerController : MonoBehaviour
 {
     
     public float speed = 1.0f;
+    public float jumpForce = 10;
+
+    private bool canJump = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            Jump();
+            canJump = false;
+        }
         transform.Translate(0,0, speed * Time.deltaTime);
 
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.transform.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+
+    private void Jump()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+        rigidBody.AddForce(0, jumpForce, 0, ForceMode.Impulse);
     }
 }
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trap : MonoBehaviour
+{
+        private IEnumerator OnCollisionEnter(Collision other)
+        { 
+                Destroy(other.gameObject);
+                yield return new WaitForSeconds(2);
+                SceneManager.LoadScene(0);
+        }
+}
