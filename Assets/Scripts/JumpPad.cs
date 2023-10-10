85ece9033ab534460612907e35df7089a334f08c
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-10-10 09:38:35 +0200

    added jump pad

---
+++ Assets/Scripts/JumpPad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpForce = 20;
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        if(rb == null) return;
+        var collidingVelocity = rb.velocity;
+        collidingVelocity.y = jumpForce;
+        rb.velocity = collidingVelocity;
+    }
+}
